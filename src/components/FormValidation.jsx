935ced9aad9e4c85027d8246f9469a6b0764abc9
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2024-09-14 09:23:17 +0530

    form structure created

---
+++ src/components/FormValidation.jsx
@@ -0,0 +1,32 @@
+import spotifyLogo from "../assets/logo.png";
+
+const FormValidation = () => {
+  return (
+    <div className="form-container">
+      <div className="box">
+        <div className="logo">
+          <img src={spotifyLogo} alt={spotifyLogo} />
+        </div>
+        <form>
+          <div className="form-details">
+            <input type="text" placeholder="Enter your Name" />
+          </div>
+          <div className="form-details">
+            <input type="email" placeholder="Enter your Email" />
+          </div>
+          <div className="form-details">
+            <input type="text" placeholder="Enter your Password" />
+          </div>
+          <div className="form-details">
+            <button>Sign up</button>
+          </div>
+          <div className="new-user">
+            <span>Already Registered?</span> <button>Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default FormValidation;
